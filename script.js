2e9250f2fd0ec350aec7265f82c87f6c3fb27d78
pwnyboy1 <fredi.rivera@hotmail.com>
2023-12-06 23:53:37 -0500

    Add logic for a single round of rock,paper, scissors
    create a function that takes in the computer's choice and player's
    choice. Then based on if states check both choices and assign a winner.
    Function also logs results to the console

--- script.js
+++ script.js
@@ -3,18 +3,62 @@
 //the function should return the value
 
 
-let compChoice;
+var playerInput = prompt("Chose: Rock, Paper, or Scissors").toLowerCase();
 
 function getComputerChoice(){
     compChoice = Math.floor(Math.random()* 3 + 1);
     if (compChoice === 1){
-        compChoice = "Scissors"
+        return "Scissors"
     }
     else if (compChoice === 2){
-        compChoice = "Rock"
+        return "Rock"
     }
     else {
-        compChoice = "Paper"
+        return "Paper"
     }
-    console.log(compChoice);
 }
+
+
+//open a prompt window
+//have user input their choice: rock, paper, or scissors
+//save user's input so we can use it to play against the computer
+
+function gameRound(playerSelection,computerSelection){
+    playerSelection = playerInput;
+    computerSelection = getComputerChoice();
+
+    if (playerSelection === "rock" && computerSelection === "Rock"){
+        console.log(`You picked ${playerInput}. Computer picked ${computerSelection}. It's a tie! Try again`);
+        playerSelection;
+    }
+    else if ((playerSelection === "rock") && computerSelection === "Paper"){
+        console.log(`You picked ${playerInput}. Computer picked ${computerSelection}. You lose`);
+    }
+    else if (playerSelection === "rock" && computerSelection === "Scissors"){
+        console.log(`Player picked ${playerInput}. Computer picked ${computerSelection}. You win!`);
+    }
+    else if (playerSelection === "paper" && computerSelection === "Rock"){
+        console.log(`Player picked ${playerInput}. Computer picked ${computerSelection}. You win!`);
+    }
+    else if (playerSelection === "paper" && computerSelection === "Paper"){
+        console.log(`Player picked ${playerInput}. Computer picked ${computerSelection}. It's a tie! Try again`);
+        playerSelection;
+    }
+    else if (playerSelection === "paper" && computerSelection === "Scissors"){
+        console.log(`Player picked ${playerInput}. Computer picked ${computerSelection}. You lose!`);
+    }
+    else if (playerSelection === "scissors" && computerSelection === "Rock"){
+        console.log(`Player picked ${playerInput}. Computer picked ${computerSelection}. You lose!`);
+    }
+    else if (playerSelection === "scissors" && computerSelection === "Paper"){
+        console.log(`Player picked ${playerInput}. Computer picked ${computerSelection}. You win!`);
+    }
+    else if (playerSelection ==="scissors" && computerSelection === "Scissors"){
+        console.log(`Player picked ${playerInput}. Computer picked ${computerSelection}. It's a tie! Try again`);
+        playerSelection;
+    }
+    else
+        console.log("Please Try Again")
+
+}
+gameRound();
