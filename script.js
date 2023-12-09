90ed1784250517c7eec54608f6e02f68959b900b
pwnyboy1 <fredi.rivera@hotmail.com>
2023-12-09 15:04:35 -0500

    Playround function now directly compares both parameters.
    first it checks if a valid selection is made.
    then it checks for a tie by playerSelection === computerSelection.
    then it checks if the player wins.
    finally computer wins if the firsts conditions aren't met.

--- script.js
+++ script.js
@@ -16,8 +16,27 @@ function getComputerChoice(){
     return choices(randomIndex);
 }
 
+// now the function directly compares both parameters.
+// first it checks if a valid selection is made
+// then it checks for a tie by playerSelection === computerSelection
+// then it checks if the player wins.
+// finally computer wins if the firsts conditions aren't met
 
 function playRound(playerSelection,computerSelection){
-
+    playerSelection = playerSelection.toLowerCase();
+    if (!["rock","paper", "scissors"].includes(playerSelection)){
+    return "Please make valid selection. Choose rock, paper, or scissors.";  
 }
-
+    if (playerSelection === computerSelection){
+        return `It's a tie! Both chose ${playerSelection}. Play again.`;
+    }
+    else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper") 
+    ){
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else{
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    }
+}
